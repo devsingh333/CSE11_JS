b86be6942f9c3e8e7c729ca62ac905150b1e5f8b
DEV SINGH <devsingh120307@gmail.com>
2026-03-10 09:39:43 +0530

    c3

---
+++ c3.js
@@ -0,0 +1,38 @@
+// normal function 
+function add(a,b){
+    console.log(a+b);
+}
+
+// arrow function 
+let addition = (a,b) =>{
+    console.log(a+b);
+}
+
+// object :
+let person2 = new Object();
+person2.name = "dev";
+person2.age = 19;
+person2.isMarried = false;
+person2.address = {};
+person2.address.street = "69A church street";
+
+// array :
+// const myInfo = [];
+// const myInfoRef = myInfo;
+
+// var myInfo = new Array();
+// var myInfo1 = Array();
+// console.log(myInfo,myInfo1);
+
+let collection = {
+    {},
+    [],
+    true,
+    "john",
+    function(){},
+    90009090567,
+    undefined,
+    null,
+    new String("abc"),
+    new DataTransfer(),
+};
